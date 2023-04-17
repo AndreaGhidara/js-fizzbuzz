c3b2846b96a8729d3d0128385bf806fe45309921
Andrea Ghidara <Ghidara.andrea.99@gmail.com>
2023-04-17 16:30:45 +0200

    finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,41 +5,45 @@ document.querySelector("body").append(container);
 for (var i = 1; i < 101; i++) {
     let box = document.createElement("div");
     box.className = "box";
+    box.style.backgroundSize = "contain"
 
     if (i % 15 == 0) {
         let textBox = document.createTextNode("fizzBuzz");
-        box.appendChild(textBox);
+        box.append(textBox);
         
         document.getElementById("container").append(box);
-        box.style.backgroundColor = "Green"
-        
+        box.style.backgroundColor = "green"
+        box.style.backgroundImage = `url("https://storage.googleapis.com/sticker-prod/TfbAxbImliJyPpOosUyn/8.thumb128.webp")`;
         // console.log("FizzBuzz");
     }
 
     else if (i % 3 == 0){
         let textBox = document.createTextNode("fizz");
-        box.appendChild(textBox);
+        box.append(textBox);
         
         document.getElementById("container").append(box);
+        box.style.backgroundImage = `url("https://storage.googleapis.com/sticker-prod/TfbAxbImliJyPpOosUyn/0.thumb128.webp")`;
         box.style.backgroundColor = "red"
+        
         
         console.log("Fizz");
     }
 
     else if (i % 5 == 0) {
         let textBox = document.createTextNode("buzz");
-        box.appendChild(textBox);
+        box.append(textBox);
         
         document.getElementById("container").append(box);
-        box.style.backgroundColor = "Blue"
-        
+        box.style.backgroundImage = `url("https://storage.googleapis.com/sticker-prod/TfbAxbImliJyPpOosUyn/1.thumb128.webp")`;
+        box.style.backgroundColor = "blue";
+
         console.log("Buzz");
     }
     
 
     else {
         let textBox = document.createTextNode(i);
-        box.appendChild(textBox);
+        box.append(textBox);
         
         document.getElementById("container").append(box);
         
